371ac6eb51384da3bb0567de7144f7405ebcdc48
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-09 07:29:14 -0500

    Define the dependencies to tetoca project

---
+++ src/main/java/com/tetoca/tetoca_api/TetocaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tetoca.tetoca_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetocaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetocaApiApplication.class, args);
+	}
+
+}
