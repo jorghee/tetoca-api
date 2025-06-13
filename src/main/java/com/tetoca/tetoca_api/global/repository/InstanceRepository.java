70f312ea7e2c3da768ca687c09f311e692d4e084
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-13 09:01:50 -0500

    New methods to access database

--- src/main/java/com/tetoca/tetoca_api/global/repository/InstanceRepository.java
+++ src/main/java/com/tetoca/tetoca_api/global/repository/InstanceRepository.java
@@ -3,7 +3,9 @@ package com.tetoca.tetoca_api.global.repository;
 import com.tetoca.tetoca_api.global.entity.InstanceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InstanceRepository extends JpaRepository<InstanceEntity, Long> {
+import java.util.Optional;
 
-  InstanceEntity findByDbName(String dbName);
+public interface InstanceRepository extends JpaRepository<InstanceEntity, Long> {
+  Optional<InstanceEntity> findByDbNameIgnoreCase(String dbName);
+  Optional<InstanceEntity> findByCompanyId(Long companyId);
 }
