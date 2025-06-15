fdac107f95c9aa35fc65ae1c4ff34bf47ddcce9e
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-15 17:36:10 -0500

    Ignore auto-configurations from Hibernate

--- src/main/java/com/tetoca/tetoca_api/TetocaApiApplication.java
+++ src/main/java/com/tetoca/tetoca_api/TetocaApiApplication.java
@@ -3,7 +3,13 @@ package com.tetoca.tetoca_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+  exclude = {
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+  }
+)
 public class TetocaApiApplication {
 
 	public static void main(String[] args) {
