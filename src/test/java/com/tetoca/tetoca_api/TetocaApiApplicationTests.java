89d12bd4c031cf3d7c48fe8688fb9b99a6cd0b5e
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-15 11:24:17 -0500

    Set default setting for time

--- src/test/java/com/tetoca/tetoca_api/TetocaApiApplicationTests.java
+++ src/test/java/com/tetoca/tetoca_api/TetocaApiApplicationTests.java
@@ -2,10 +2,8 @@ package com.tetoca.tetoca_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class TetocaApiApplicationTests {
 
 	@Test
