5cdc969ec68d881cfe5a658ec17853a39c71ab82
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-22 15:11:04 -0500

    Import jakarta.validation.constraints.Size

--- src/main/java/com/tetoca/tetoca_api/global/dto/company/InstanceRequest.java
+++ src/main/java/com/tetoca/tetoca_api/global/dto/company/InstanceRequest.java
@@ -2,6 +2,7 @@ package com.tetoca.tetoca_api.global.dto.company;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
